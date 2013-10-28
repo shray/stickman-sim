b2e0c0b801050660171a230ea1c1a425a9192421
Shray Bansal <shray.bansal@gmail.com>
2013-10-28 19:02:33 -0400

    Cleaned up code. A few more comments. Better orto-graphic visuals. Tracking ofcourse still does not work.

--- stick_man.m
+++ stick_man.m
@@ -1,26 +1,36 @@
 %%  Simulate the physical system -- Stickman
 clear
 close all
+
+%initialize RNG
+rng(1,'twister');
+
 %constants
 STICK_LEN = 5; %length in axis of rotation
 STICK_RATIOS = [.5, .5]; ARM_RATIOS =[ .2, .2]; % ratio to len (arms-intersection, armr, arml)
-OCCLUDE_RADIUS = 0.0001;
+
+%Occlusion caused by following - scaled by distance from camera
+OCCLUDE_RADIUS = 0.1; %Only for the ends
+OCCLUDE_WIDTH = 0; %For the arms and the stick - considering cylinder
 
 start_arms_angles = [45, -45]; %degrees
-vel_stick_rotation = 5; %deg/frame
+
+vel_stick_rotation = [5 0]; %(mean, std) deg/frame
+
 arm_rot = [0 5; 0 5]; %(mean,std) gauss to sample rotation speed
-no_arms = 2;
+no_arms = 2; %number of arms
 
 % points that are projected are two ends of the stick
 % state of stick - apart from constants
 % assuming a coordinate axis with origin at end 1 and z direction pointing
 % pointing towards end1 and x in arm-left
-theta = start_arms_angles';
-stick_ends = [0 0 0; 0 0 STICK_LEN]';
 
+%initialiaze stick-man
+theta = start_arms_angles'; %angle between each of the arms and stick
+stick_ends = [0 0 0; 0 0 STICK_LEN]'; %3d pos of stick in own frame
+phi = 0; %angle of the stick's xy plane to that of world
 arm_starts = zeros(3, no_arms);
 arm_ends = zeros(3, no_arms);
-
 for i =1:no_arms
     arm_starts(:,i) = (stick_ends(:,2)-stick_ends(:,1)).*STICK_RATIOS(i);
     arm_ends(:,i) =  arm_starts(:,i) + (STICK_LEN*ARM_RATIOS(i)) ...
@@ -29,20 +39,51 @@ end
 
 
 %Tracking initials
-lengths = STICK_LEN * [1; SICK_RATIOS(1); ARM_RATIOS(1)];
+lengths = STICK_LEN * [1; STICK_RATIOS(1); ARM_RATIOS(1)];
 chosen_cam = 1;
 
-%start rotating STICK-MAN
-for phi=0:vel_stick_rotation:0
+total_frame=10;
+%start STICK-MAN dynamics
+for cur_frame=0:total_frame
 
-%orthographic projection
-%first translate-rotate into world coordinates, then take the (x,z)
-translation_1 = [0 -5 0]';
+translation_1 = [0 -5 0]'; %translation from stick origin to world in world coor
 
-% phi = 0;
+%rotate stick - according to velocity subject to less than 360
+phi = mod(phi + vel_stick_rotation(1) + vel_stick_rotation(2)*randn(),360);
+%rotate by phi
 phi_rot = [cosd(-phi) -sind(-phi) 0;
            sind(-phi) cosd(-phi)  0;
                 0       0        1];
+            
+%change theta - or  start waving
+% theta = [start_arms_angles(1)+theta_del start_arms_angles(2)-theta_del];
+theta = theta + arm_rot(:,1) + arm_rot(:,2) .* randn(size(theta));
+
+%TODO: generalize
+%checks that arms don't rotate around towards the end for either end
+if theta(1)>179 
+    theta(1)=179;
+elseif theta(1)<1
+        theta(1)=1;
+end
+
+if theta(2)<-179 
+    theta(2)=-179;
+elseif theta(2)>-1
+        theta(2)=-1;
+end
+
+%currently the stick-ends are fixed - could make it a random walk later
+stick_ends = [0 0 0; 0 0 STICK_LEN]';
+
+arm_starts = zeros(3, no_arms);
+arm_ends = zeros(3, no_arms);
+
+for i = 1:no_arms
+    arm_starts(:,i) = (stick_ends(:,2)-stick_ends(:,1)).*STICK_RATIOS(i);
+    arm_ends(:,i) =  arm_starts(:,i) + (STICK_LEN*ARM_RATIOS(i)) ...
+                        .* [cosd(90-theta(i)) 0 sind(90-theta(i))]';
+end            
            
 %change to world frame
 world_stick_ends = phi_rot*stick_ends + repmat(translation_1,1,no_arms);
@@ -50,51 +91,28 @@ world_arm_starts = phi_rot*arm_starts + repmat(translation_1,1,no_arms);
 world_arm_ends = phi_rot*arm_ends + repmat(translation_1,1,no_arms);
 
     
-%draw
+%draw - orthographic projection
 clf
 figure(2)
-xlim([-5 5]);
-ylim([-5 5]);
+xlim([-3 3]);
+ylim([-1 7]);
 line([world_stick_ends(1,1);world_stick_ends(1,2)],...
        [world_stick_ends(3,1);world_stick_ends(3,2)]);
-   for i  = 1:no_arms
+   color_name=[];
+   for i=1:no_arms
     hold on
+    if mod(i,2)
+        color_name='r';
+    else
+        color_name='g';
+    end
+    
     line([[world_arm_starts(1,i)]'; [world_arm_ends(1,i)]'], ...
-        [[world_arm_starts(3,i)]'; [world_arm_ends(3,i)]']);
+        [[world_arm_starts(3,i)]'; [world_arm_ends(3,i)]'], 'Color', color_name);
+    
    end
    
-   pause(0.3)
-   
-
-%change theta
-% theta = [start_arms_angles(1)+theta_del start_arms_angles(2)-theta_del];
-theta = theta + arm_rot(:,1) + arm_rot(:,2) .* randn(size(theta));
-
-%TODO: generalize
-if theta(1)>179 
-    theta(1)=179;
-elseif theta(1)<1
-        theta(1)=1;
-end
-        
-if theta(2)<-179 
-    theta(2)=-179;
-elseif theta(2)>-1
-        theta(2)=-1;
-end
-
-stick_ends = [0 0 0; 0 0 STICK_LEN]';
-
-arm_starts = zeros(3, no_arms);
-arm_ends = zeros(3, no_arms);
-
-for i =1:no_arms
-    arm_starts(:,i) = (stick_ends(:,2)-stick_ends(:,1)).*STICK_RATIOS(i);
-    arm_ends(:,i) =  arm_starts(:,i) + (STICK_LEN*ARM_RATIOS(i)) ...
-                        .* [cosd(90-theta(i)) 0 sind(90-theta(i))]';
-end
-
-
+   pause(0.3)   
 end
 
 %only care about the image of these 3d homogenous points
@@ -104,7 +122,7 @@ care_points = [world_stick_ends world_arm_ends;...
 
 
 
-%% Camera projection1 - pinhole
+%% Camera1 projection - pinhole
 f1 = 0.005;
 camera1_mat = [f1 0 0 0;
               0 f1 0 0;
@@ -116,10 +134,6 @@ camera1_rot  = [1    0       0
                                                     
 cam1_to_world = [0 0 0]';
 
-%care about points
-% care_points = [world_stick_ends world_arm_starts world_arm_ends;...
-%                 ones(1,size(world_stick_ends,2)+...
-%                 size(world_arm_ends,2)+size(world_arm_starts,2))];
 transform_cam1_frame = [[camera1_rot; zeros(1,3)] [cam1_to_world; 1]];
 
 %project point to camera space
